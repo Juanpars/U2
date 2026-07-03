258f4431356858cdd706887da59f4399555cbdb3
juanpars <jrps4949@gmail.com>
2026-07-03 12:22:09 -0600

    Unidad 2

---
+++ jp-app/src/app/shared/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  readonly authService = inject(AuthService);
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+}
