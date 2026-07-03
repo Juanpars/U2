652cdc6c3f2acd0d4b32b4694571f534e3ecd648
juanpars <jrps4949@gmail.com>
2026-07-03 12:25:23 -0600

    Convertir jp-api de submódulo a directorio normal

---
+++ jp-api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './task/infrastructure/task.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    AuthModule,
+    TasksModule
+  ],
+})
+export class AppModule {}
